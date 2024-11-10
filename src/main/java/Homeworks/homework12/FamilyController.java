f1ee8697b29737f699bff492628ab93867ca578a
Martynenko Maksym <super.super.maksim33@gmail.com>
2024-11-10 15:08:38 +0100

    homework 12 done

--- src/main/java/Homeworks/homework12/FamilyController.java
+++ src/main/java/Homeworks/homework12/FamilyController.java
@@ -1,4 +1,81 @@
 package Homeworks.homework12;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class FamilyController {
+    FamilyService fs = new FamilyService();
+    Random rand = new Random();
+
+    public ArrayList<Family> getAllFamilies() {
+        return fs.getAllFamilies();
+    }
+
+    public void displayAllFamilies() {
+        int familyIndex = 1;
+        for (Family family : fs.getAllFamilies()) {
+            System.out.printf("Family #%d", familyIndex);
+            System.out.println();
+            System.out.println(family.prettyFormat());
+            familyIndex++;
+        }
+    }
+
+    public void getFamiliesBiggerThan(int x) {
+        fs.getAllFamilies().stream()
+                .filter(k -> k.countMembers() > x)
+                .forEach(System.out::println);
+    }
+
+    public void getFamiliesLessThan(int x) {
+        fs.getAllFamilies().stream()
+                .filter(k -> k.countMembers() < x)
+                .forEach(System.out::println);
+    }
+
+    public long countFamiliesWithMemberNumber(int x) {
+        return fs.getAllFamilies().stream()
+                .filter(k -> k.countMembers() == x)
+                .count();
+    }
+
+    public void createNewFamily(Human mother, Human father) {
+        fs.getAllFamilies().add(new Family(mother, father));
+    }
+
+    public void deleteFamily(Family family) {
+        fs.getAllFamilies().remove(family);
+    }
+
+    public void deleteFamilyByIndex(int index) {
+        fs.getAllFamilies().remove(index);
+    }
+
+    public void bronChild(int index, String nameBoy, String nameGirl) {
+        String name;
+        name = rand.nextInt(2) + 1 == 1 ? nameBoy : nameGirl;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        String birthDate = LocalDate.now().format(formatter);
+        fs.getAllFamilies().get(index).addChild(new Human(name, fs.getAllFamilies().get(index).getFather().getSurname(), birthDate, rand.nextInt(150) + 70));
+    }
+
+    public void adoptChild(int index, Human human) {
+        fs.getAllFamilies().get(index).addChild(human);
+    }
+
+    public void deleteAllChildrenOlderThan(int age) {
+        for (Family family : fs.getAllFamilies())
+            family.deleteAllChildrenOlderThan(age);
+    }
+
+    public int count(){
+        return fs.getAllFamilies().size();
+    }
+
+    public Family getFamily(int index){
+        return fs.getAllFamilies().get(index);
+    }
+
 }
--- src/main/java/Homeworks/homework12/HappyFamily.java
+++ src/main/java/Homeworks/homework12/HappyFamily.java
@@ -3,11 +3,18 @@ package Homeworks.homework12;
 public class HappyFamily {
     public static void main(String[] args) {
 
-        FamilyService familyService = new FamilyService();
+        FamilyController fc = new FamilyController();
         Human h1 = new Human("1", "1", "1000/10/10", 10);
         Human h2 = new Human("2", "2", "1000/10/10", 20);
-        familyService.createNewFamily(h1, h2);
-        familyService.displayAllFamilies();
-        familyService.displayAllFamilies1();
+        fc.createNewFamily(h1, h2);
+        fc.createNewFamily(h2, h1);
+        fc.adoptChild(0, h1);
+        fc.displayAllFamilies();
+        fc.getFamiliesBiggerThan(2);
+        fc.getFamiliesLessThan(3);
+        System.out.println(fc.countFamiliesWithMemberNumber(2));
+        System.out.println(fc.count());
+        fc.deleteFamilyByIndex(0);
+        System.out.println(fc.count());
     }
 }
